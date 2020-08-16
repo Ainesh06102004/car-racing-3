2ee2ed7f376e20bac426c839e2d7653ef9cc0e73
Ainesh Nanda <67788476+Ainesh06102004@users.noreply.github.com>
2020-08-16 05:25:28 -0700

    car race

---
+++ JS/form.js
@@ -0,0 +1,40 @@
+class Form{
+    constructor(){
+        this.title = createElement('h2',"MULTIPLAYER CAR RACING GAME");
+        this.input = createInput();
+        this.button = createButton("PLAY");
+        this.checkbox = createCheckbox("I agree to all Terms & Conditions", false);
+        }
+    hide(){
+        this.title.hide();
+        this.greeting.hide();
+        
+    }
+    display(){
+        this.title.position(displayWidth/2 - 220, 100);
+        this.input.position(displayWidth/2 - 100,300);
+        this.button.position(displayWidth/2 - 40,350);
+        this.checkbox.position(displayWidth/2 - 135,400);
+
+        this.checkbox.changed(()=>{
+            console.log("pressed");
+            this.button.mousePressed(()=>{
+                console.log("mouse pressed");
+                this.input.hide();
+                this.button.hide();
+                this.checkbox.hide();
+                player.name = this.input.value();
+                player.getPlayercount();
+                console.log(playercount);
+                playercount = playercount +1;
+                player.index = playercount;
+                player.update();
+                player.updatePlayercount(playercount);
+                this.greeting = createElement('h2', "Welcome "+ player.name);
+                this.greeting.position(displayWidth/2 - 100,300);
+            });
+        });
+    }
+
+
+}
---
+++ sketch.js
@@ -0,0 +1,40 @@
+var database;
+var form, game;
+var playercount = 0;
+var player, allplayers;
+var gamestate = 0;
+var car1,car2,car3,car4,cars;
+var car1img,car2img,car3img,car4img,groundimg,trackimg;
+
+function preload(){
+  car1img = loadImage("images/car1.png");
+  car2img = loadImage("images/car2.png");
+  car3img = loadImage("images/car3.png");
+  car4img = loadImage("images/car4.png");
+  groundimg = loadImage("images/ground.png");
+  trackimg = loadImage("images/track.jpg");
+}
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(displayWidth - 5,displayHeight - 150);
+
+  game = new Game();
+  game.getstate();
+  game.start();
+
+}
+
+function draw(){
+  background("white");
+  
+ 
+
+  if(playercount === 4){
+    game.updatestate(1);
+  }
+   if(gamestate === 1){
+     game.play();
+   }
+}
